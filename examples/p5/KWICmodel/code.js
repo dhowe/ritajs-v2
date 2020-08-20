a7b5c1ea8a203b5887e5777001c8ba9cde95b61b
System Administrator <root@zhangyude-MacBook-Pro.local>
2020-08-20 15:33:10 +0800

    redowithP5createButton

--- examples/p5/KWICmodel/code.js
+++ examples/p5/KWICmodel/code.js
@@ -11,11 +11,14 @@ function preload() {
 function setup() {
 
   createCanvas(800, 500);
-  textFont('Times');
-  textSize(18);
+  textFont('Times New Roman');
+  textSize(16);
+  //to keep the same style as the dom example
 
   RiTa.concordance(txt.join('\n'));
   word = RiTa.random(keywords);
+
+  makeButtons()
 }
 
 function draw() {
@@ -23,7 +26,7 @@ function draw() {
   let kwic = RiTa.kwic(word, 6);
 
   background(250);
-  drawButtons();
+  //drawButtons();
 
   let tw = textWidth(word) / 2;
 
@@ -49,50 +52,85 @@ function draw() {
   noLoop();
 }
 
+//using p5js createButton()
+
+function makeButtons(){
+  let buttonNo = keywords.length, gapNo = keywords.length-1;
+  let gapWidth = 10, buttonPadding = 10, yPosition = 65, buttonWidthInTotal = 0;
+  let buttonArray = [];
+  for (let i = 0; i < buttonNo; i++){
+    let newButton = createButton(keywords[i]);
+    buttonWidthInTotal += newButton.width;
+    buttonArray.push(newButton);
+  }
+  let toAdd = 0, startX = width / 2 - (gapNo * gapWidth + buttonWidthInTotal) / 2;
+  for (let i = 0; i < buttonNo; i++){
+    buttonArray[i].position(startX+toAdd,yPosition);
+    buttonArray[i].class("button");
+    if (word === keywords[i]){
+      buttonArray[i].style('color','rgb(200,0,0)');
+    }
+    toAdd += buttonArray[i].width + gapWidth;
+  }
+  for (let i = 0; i < buttonNo; i++){
+    buttonArray[i].mouseClicked(function() {
+      word = keywords[i];
+      for (j = 0; j < keywords.length; j++){
+        if (i === j){
+          buttonArray[j].style('color','rgb(200,0,0)');
+        } else {
+          buttonArray[j].style('color','black');
+        }
+      }
+      loop();
+    });
+  }
+}
+
 // TODO: remove below and replace with p5js createButton()
 
-function getButtonX() {
-  let gapWidth = 10, buttonPadding = 10;
-  let gapNo = keywords.length - 1, twInTotal = 0;
-  for (let i = 0; i < keywords.length; i++) {
-    twInTotal += textWidth(keywords[i]);
-  }
-  return width / 2 - (gapNo * gapWidth +
-    buttonPadding * keywords.length + twInTotal) / 2
-}
-
-function drawButtons() {
-  let posX = getButtonX();
-  for (let i = 0; i < keywords.length; i++) {
-    let on = word === keywords[i] ? true : false;
-    let tw = textWidth(keywords[i]);
-    stroke(200);
-    fill(255);
-    rect(posX - 5, 24, tw + 10, 20, 7);
-
-    // change color for enabled button
-    fill((on ? 200 : 0), 0, 0);
-    text(keywords[i], posX, 40);
-
-    posX += tw + 20;
-  }
-}
-
-function inside(mx, my, posX, tw) {
-  return (mx >= posX - 5 && mx <= posX + tw + 5
-    && my >= 25 && my <= 44);
-}
-
-function mouseClicked() {
-
-  let posX = getButtonX(), tw;
-  for (let i = 0; i < keywords.length; i++) {
-    tw = textWidth(keywords[i]);
-    if (inside(mouseX, mouseY, posX, tw)) {
-      word = keywords[i];
-      loop(); // re-render
-      break;
-    }
-    posX += tw + 20;
-  }
-}
+// function getButtonX() {
+//   let gapWidth = 10, buttonPadding = 10;
+//   let gapNo = keywords.length - 1, twInTotal = 0;
+//   for (let i = 0; i < keywords.length; i++) {
+//     twInTotal += textWidth(keywords[i]);
+//   }
+//   return width / 2 - (gapNo * gapWidth +
+//     buttonPadding * keywords.length + twInTotal) / 2
+// }
+//
+// function drawButtons() {
+//   let posX = getButtonX();
+//   for (let i = 0; i < keywords.length; i++) {
+//     let on = word === keywords[i] ? true : false;
+//     let tw = textWidth(keywords[i]);
+//     stroke(200);
+//     fill(255);
+//     rect(posX - 5, 24, tw + 10, 20, 7);
+//
+//     // change color for enabled button
+//     fill((on ? 200 : 0), 0, 0);
+//     text(keywords[i], posX, 40);
+//
+//     posX += tw + 20;
+//   }
+// }
+//
+// function inside(mx, my, posX, tw) {
+//   return (mx >= posX - 5 && mx <= posX + tw + 5
+//     && my >= 25 && my <= 44);
+// }
+//
+// function mouseClicked() {
+//
+//   let posX = getButtonX(), tw;
+//   for (let i = 0; i < keywords.length; i++) {
+//     tw = textWidth(keywords[i]);
+//     if (inside(mouseX, mouseY, posX, tw)) {
+//       word = keywords[i];
+//       loop(); // re-render
+//       break;
+//     }
+//     posX += tw + 20;
+//   }
+// }
