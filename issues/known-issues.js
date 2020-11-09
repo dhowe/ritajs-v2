e19e6d199b167433d431816cd31b449e370ceb46
John C <real.john.chueng@gmail.com>
2020-11-09 22:12:01 +0800

    delete pass tests in known issues

--- issues/known-issues.js
+++ issues/known-issues.js
@@ -30,33 +30,6 @@ describe('RiScript.KnownIssues', () => { // TODO:
     expect(rs).to.be.oneOf(["Dave talks to Dave.", "Jill talks to Jill.", "Pete talks to Pete."]);
   });
 
-  it('1: pluralize or singularize fails', () => {
-    let testPairs = []; // SS FAILING ITEMS HERE
-    let res1, res2, res3, i = 0, dbug = true;
-    if (!testPairs.length) return;
-    dbug && console.log(testPairs[i] + '/' + testPairs[i + 1]);
-
-    res1 = RiTa.singularize(testPairs[i], { dbug: dbug });
-    res2 = RiTa.pluralize(testPairs[i + 1], { dbug: dbug });
-    res3 = RiTa.inflector.isPlural(testPairs[i], { dbug: dbug });
-
-    eq(res1, testPairs[i + 1], 'FAIL: singularize(' + testPairs[i]
-      + ') was ' + res1 + ', but expected ' + testPairs[i + 1] + '\n        '
-      + 'pluralize(' + testPairs[i + 1] + ') was ' + res2 + '\n\n');
-
-    // pluralize
-    eq(res2, testPairs[i], 'FAIL: pluralize(' + testPairs[i + 1]
-      + ') was ' + res2 + ', but expected ' + testPairs[i] + '\n        '
-      + 'singularize(' + testPairs[i] + ') was ' + res1 + '\n\n');
-
-    ok(res3, 'FAIL: isPlural(' + testPairs[i] + ') was false\n\n');
-  });
-
-  0 && it('simple evaluations', function () {
-    // should this throw? maybe yes: can do foo&dot;bar - need better error
-    expect(RiTa.evaluate('foo.bar', {}, 1)).eq('foo.bar');
-  });
-
   it('Should eval simple expressions', () => {
     // NOT SURE WHAT THIS TEST IS ABOUT
     expect(RiTa.evaluate('$foo=bar \\nbaz\n$foo', {}, TT)).eq('bar baz'); ``
@@ -71,96 +44,10 @@ describe('RiScript.KnownIssues', () => { // TODO:
 
 describe('RiTa.KnownIssues', () => {
 
-  it('Failing to singularize correctly single', () => {
-
-    let testPairs = ["grooves", "groove" ];
-
-    let res1, res2, res3, dbug = 1;
-
-    for (let i = 0; i < testPairs.length; i += 2) {
-
-      dbug && console.log(testPairs[i] + '/' + testPairs[i + 1]);
-
-      res1 = RiTa.singularize(testPairs[i], { dbug: dbug });
-      res2 = RiTa.pluralize(testPairs[i + 1], { dbug: dbug });
-      res3 = RiTa.inflector.isPlural(testPairs[i], { dbug: dbug, fatal: false });
-
-      // singularize
-      eq(res1, testPairs[i + 1], 'FAIL: singularize(' + testPairs[i]
-        + ') was ' + res1 + ', but expected ' + testPairs[i + 1] + '\n        '
-        + 'pluralize(' + testPairs[i + 1] + ') was ' + res2 + '\n\n');
-
-      // pluralize
-      eq(res2, testPairs[i], 'FAIL: pluralize(' + testPairs[i + 1]
-        + ') was ' + res2 + ', but expected ' + testPairs[i] + '\n        '
-        + 'singularize(' + testPairs[i] + ') was ' + res1 + '\n\n');
-
-      // isPlural
-      ok(res3, 'FAIL: isPlural(' + testPairs[i] + ') was false\n\n');
-    }
-  });
-
-  it('Failing to singularize correctly', () => {
+  it('Failing to pluralize correctly', () => {
 
     let testPairs = [ // also in java
-      "grooves", "groove",
-      "universes", "universe",
-      "toothbrushes", "toothbrush",
-      "clashes", "clash",
-      "addresses", "address",
-      "flashes", "flash",
-      "conclaves", "conclave",
-      "promises", "promise",
-      "spouses", "spouse",
-      "branches", "branch",
-      "lapses", "lapse",
-      "quizes", "quiz",
-      "spyglasses", "spyglass",
-      "overpasses", "overpass",
-      "clones", "clones",
-      "microwaves", "microwave",
-      "hypotheses", "hypothesis",
-      "pretenses", "pretense",
-      "latches", "latch",
-      "fetuses", "fetus",
-      "alumni", "alumnus",
-      "lighthouses", "lighthouse",
-      "genuses", "genus",
-      "zombies", "zombie",
-      "hearses", "hearse",
-      "trenches", "trench",
-      "paradoxes", "paradox",
-      "hippies", "hippie",
-      "yuppies", "yuppie",
-      "purses", "purse",
-      "hatches", "hatch",
-      "witches", "witch",
-      "sinuses", "sinus",
-      "phrases", "phrase",
-      "arches", "arch",
-      "duplexes", "duplex",
-      "missives", "missive",
-      "madhouses", "madhouse",
-      "pauses", "pause",
-      "heroes", "hero",
-      "sketches", "sketch",
-      "meshes", "mesh",
-      "brasses", "brass",
-      "marshes", "marsh",
-      "masses", "mass",
-      "overpasses", "overpass",
-      "impulses", "impulse",
-      "pelvises", "pelvis",
-      "fetishes", "fetish",
-      "abysses", "abyss",
-      "lighthouses", "lighthouse",
-      "gashes", "gash",
-      "directives", "directive",
-      "calories", "calorie",
-      "moves", "move",
-      "expanses", "expanse",
-      "briefcases", "briefcase",
-      "pleae", "pleae",
+      "pleae", "pleae",// can't find it in dictionary
     ];
 
     let res1, res2, res3, dbug = 0;
