660d3cca7121f5cb7d627457aa9e619a521d1ea9
dhowe <dhowe@cims.nyu.edu>
2020-11-11 17:08:36 +0800

    update script to match new java dict location (for maven)

--- scripts/update-java-dict.sh
+++ scripts/update-java-dict.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp src/rita_dict.js ../rita2/src/main/java/rita/
-ls -l src/rita_dict.js ../rita2/src/main/java/rita/rita_dict.js
+cp src/rita_dict.js ../rita2/src/main/resources/rita/
+ls -l src/rita_dict.js ../rita2/src/main/resources/rita/rita_dict.js
