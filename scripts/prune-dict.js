4db4ce8e460ceea32ffcd106331fb2e25145e1f1
dhowe <dhowe@cims.nyu.edu>
2021-01-01 19:13:59 +0800

    remove vbg only from dict

---
+++ scripts/prune-dict.js
@@ -0,0 +1,16 @@
+let dict = require("./src/rita_dict.js");
+let words = Object.keys(dict), result = {};
+let num = words.length, hits = 0;
+words.forEach(w => {
+  let pos = dict[w][1];
+  if (pos !== 'vbg') result[w] = dict[w];
+  else hits++;
+});
+
+let str = JSON.stringify(result).replace(/\],/g,"],\n  ");
+str = str.replace(/{/,"module && (module.exports = {\n  ").replace(/}/,"\n});");
+require('fs').writeFile('newdict.js', str, (err) => {
+  if (err) return console.log(err);
+  console.log('Pruned '+hits+" from "+num+" words, "+Object.keys(result).length+" remaining");
+});
+
