c11b2382ebd5d32e64daf4b5f2d617be8125e16c
dhowe <dhowe@cims.nyu.edu>
2021-01-07 19:55:59 +0800

    testing umd configs

---
+++ config/webpack.umd.deps.js
@@ -0,0 +1,25 @@
+
+// node/full
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: require('path').resolve(__dirname, '../dist'),
+    library: 'RiTa',
+    filename: 'rita.umd.js',
+    libraryTarget: 'umd',
+  },  
+  node: {
+    fs: "empty",
+    __dirname: false,
+    __filename: false,
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  entry: './src/rita.js',
+  plugins: [new (require('webpack').DefinePlugin)({
+    __VERSION__: JSON.stringify(require("../package.json").version)
+  })]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
 
 // build them all
 module.exports = [
-  /* "main": "dist/rita.js",           // node.js: cjs 
-  "module": "dist/rita-es6.js",    // bundler: es2018 
-  "unpkg": "dist/rita-web.js",      // unpkg.com  */
-
   require('./config/webpack.umd'),
+  require('./config/webpack.umd.deps'),
   require('./config/webpack.web'),
   require('./config/webpack.web.nolex'),
-  //require('./config/webpack.node')
 ];
