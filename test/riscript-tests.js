88d69efd51eeb0ff666c423287369eabe316891e
System Administrator <root@zhangyude-MacBook-Pro.local>
2021-02-02 21:19:41 +0800

    remove SYNC marks

--- test/riscript-tests.js
+++ test/riscript-tests.js
@@ -156,7 +156,7 @@ describe('RiTa.RiScript', () => {
     })
   });
 
-  describe('Sequences', () => { // SYNC:
+  describe('Sequences', () => { 
     /*
     1. "$$names=(jane | dave | rick | chung)\n"
        "This story is about $names and $names.nr()"
@@ -241,7 +241,7 @@ describe('RiTa.RiScript', () => {
       expect(rs.isParseable("&&b")).eq(false);
     });
 
-    it('Should resolve simple expressions', () => { // SYNC:
+    it('Should resolve simple expressions', () => { 
 
       expect(RiTa.evaluate('foo', {})).eq('foo');
       expect(RiTa.evaluate('foo!', {})).eq('foo!');
@@ -581,7 +581,7 @@ describe('RiTa.RiScript', () => {
       expect(ctx.foo).eq('a');
     });
 
-    it('Should pluralize phrases', () => { // SYNC:
+    it('Should pluralize phrases', () => { 
       expect(RiTa.evaluate('These (bad feeling).pluralize().')).eq('These bad feelings.');
       expect(RiTa.evaluate('She (pluralize).pluralize().')).eq('She pluralizes.');
       expect(RiTa.evaluate('These ($state feeling).pluralize().', { state: 'bad' })).eq('These bad feelings.');
@@ -594,7 +594,7 @@ describe('RiTa.RiScript', () => {
       expect(RiTa.evaluate('These (off-site).pluralize().', { state: '(bad | bad)' })).eq('These off-sites.');
     })
 
-    it('Should resolve transforms on literals', () => { // SYNC:
+    it('Should resolve transforms on literals', () => { 
       expect(RiTa.evaluate('How many (teeth).quotify() do you have?')).eq('How many “teeth” do you have?');
       expect(RiTa.evaluate('That is (ant).articlize().', 0)).eq('That is an ant.');
       expect(RiTa.evaluate('That is ().articlize().', 0)).eq('That is .');
@@ -920,7 +920,7 @@ describe('RiTa.RiScript', () => {
       expect(RiTa.evaluate('The $a.capitalize() dog.', {}, ST)).eq('The $a.capitalize() dog.');
     });
 
-    it('Should ignore no-op symbols in context', () => { // SYNC:
+    it('Should ignore no-op symbols in context', () => { 
       expect(RiTa.evaluate('a $foo dog', {}, ST)).eq('a $foo dog');
       expect(RiTa.evaluate('$100 is a lot of $dog.', { dog: 'terrier' }, ST)).eq('$100 is a lot of terrier.');
       expect(RiTa.evaluate('the $dog cost $100!', { dog: 'terrier' }, ST)).eq('the terrier cost $100!');
@@ -1048,7 +1048,7 @@ describe('RiTa.RiScript', () => {
       expect(() => RiTa.evaluate('(a | b) | c', 0, ST)).to.throw();
     });
 
-    it('Should resolve choices', () => { //SYNC:
+    it('Should resolve choices', () => { 
       expect(RiTa.evaluate('(|)')).eq('');
       expect(RiTa.evaluate('(a)')).eq('a');
       expect(RiTa.evaluate('(a | a)', 0)).eq('a');
