69e73335637dceaddd01bbd80e52b13116d17c4b
dhowe <dhowe@cims.nyu.edu>
2021-02-11 21:18:53 +0800

    sync

--- test/grammar-tests.js
+++ test/grammar-tests.js
@@ -3,7 +3,7 @@ describe('RiTa.RiGrammar', function() {
 
     if (typeof module !== 'undefined') require('./before');
 
-    const ST = { silent: 1 }, TP = { trace: 1 }, SP = { singlePass: 1 }, TLP = { trace: 1, traceLex: 1 };
+    const ST = { silent: 1 }, TP = { trace: 1 }, TLP = { trace: 1, traceLex: 1 };
     const RiGrammar = RiTa.RiGrammar, SKIP_FOR_NOW = true, SEQ_COUNT = 5;
 
     this.slow(200);
