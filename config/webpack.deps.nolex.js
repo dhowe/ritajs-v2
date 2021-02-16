34ab3e943d7595e03dd37bb61834d8c29863af49
dhowe <dhowe@cims.nyu.edu>
2021-02-17 01:22:43 +0800

    babel opts

--- config/webpack.deps.nolex.js
+++ config/webpack.deps.nolex.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader?compact=true']
       }
     ]
   },
