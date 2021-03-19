f7d129e6bb94b210a542276b293f6cdc1d41b680
JohnC <real.john.chueng@gmail.com>
2021-03-19 22:52:28 +0800

    Implement on sigle test

--- test/before.js
+++ test/before.js
@@ -1,3 +1,23 @@
-import RiTa from '../dist/rita';
+async function getRiTa(){
+    if (process.env.NODE_ENV === 'dev') {
+        try {
+            const RiTa = await import('../src/rita.js');
+            return RiTa.default;
+        } catch {
+            throw Error('fail to load RiTa');
+        }
+    } else {
+        try {
+            const RiTa = await import('../dist/rita.js');
+            return RiTa.default;
+        } catch {
+            throw Error('fail to load RiTa');
+        }
+    }
+}
+let RiTa;
+getRiTa().then((Module) => {
+    RiTa = Module;
+})
 import { expect } from 'chai';
 export { RiTa, expect };
---
+++ test/import-test.js
@@ -0,0 +1,7 @@
+import { RiTa, expect } from './before';
+
+describe('import', () => {
+    it('should import rita', () => {
+        console.log(RiTa.VERSION);
+    });
+});
